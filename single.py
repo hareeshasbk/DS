753b8b84d346c2d1dcaf017bd125ebfa42637ed3
hareeshasbk <149602197+hareeshasbk@users.noreply.github.com>
2024-05-10 19:10:39 +0530

    Update single.py

--- single.py
+++ single.py
@@ -703,3 +703,28 @@ insert(HashTable, 10, 'Pune')
 
 Display(HashTable)
 
+
+# linear graph
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TKAgg')
+
+x=list(range(1,10))
+plt.plot(x ,[y for y in x])
+plt.title("linear search- time complexity is O(n)")
+plt.xlabel("input")
+plt.ylabel("time")
+plt.show()
+
+# Bubble sort
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TKAgg')
+
+x=list(range(1,10))
+plt.plot(x ,[y*y for y in x], color = 'green')
+plt.title("Bubble sort- time complexity is O(n\u00b2)")
+plt.xlabel("input")
+plt.ylabel("time")
+plt.show()
